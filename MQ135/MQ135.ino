61395d9c1c10ed787045c775fc0bdabac047f43a
Damrongwit N <admin@jack.in.th>
2015-06-29 21:14:54 +0700

    MQ-135 Sensor Arduino Sketch

--- MQ135/MQ135.ino
+++ MQ135/MQ135.ino
@@ -14,7 +14,7 @@ void setup()
 {
   Serial.begin(9600);
   float rzero = gasSensor.getRZero();
-  delay(4000);
+  delay(3000);
   Serial.print("MQ135 RZERO Calibration Value : ");
   Serial.println(rzero);
 }
